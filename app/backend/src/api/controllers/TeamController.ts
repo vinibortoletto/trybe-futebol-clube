2ebcce4d2cdf4ddb4e092f66765c4a8971c9988b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-27 23:31:19 -0300

    feat: find team by id controller

--- app/backend/src/api/controllers/TeamController.ts
+++ app/backend/src/api/controllers/TeamController.ts
@@ -10,4 +10,10 @@ export default class TeamController implements ITeamController {
     const teamList = await this._service.findAll();
     return res.status(OK).json(teamList);
   }
+
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
+    const team = await this._service.findById(Number(id));
+    return res.status(OK).json(team);
+  }
 }
--- app/backend/src/api/interfaces/ITeamController.ts
+++ app/backend/src/api/interfaces/ITeamController.ts
@@ -2,4 +2,5 @@ import { Request, Response } from 'express';
 
 export default interface ITeamController {
   findAll(req: Request, res: Response): Promise<Response | void>
+  findById(req: Request, res: Response): Promise<Response | void>
 }
