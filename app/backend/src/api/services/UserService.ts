118fcaad57f23c32c004a9bf274bc3d063482067
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-28 20:22:41 -0300

    feat: should fail to login if email does not match any in database

--- app/backend/src/api/services/UserService.ts
+++ app/backend/src/api/services/UserService.ts
@@ -3,6 +3,8 @@ import { ModelStatic } from 'sequelize';
 import IUserService from '../interfaces/IUserService';
 import ILoginInfo from '../interfaces/ILoginInfo';
 import User from '../../database/models/UserModel';
+import { Unauthorized } from '../errors';
+import { invalidFields } from '../../utils/errorMessages';
 
 export default class UserService implements IUserService {
   private _model: ModelStatic<User> = User;
@@ -11,8 +13,9 @@ export default class UserService implements IUserService {
     const { email, password } = loginInfo;
     const user = await this._model.findOne({ where: { email } });
 
+    if (!user) throw new Unauthorized(invalidFields);
+
     // const isPasswordValid = bcrypt.compareSync(password, user?.password);
-    console.log(user);
 
     return password;
   }
