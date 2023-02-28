b75694bb7a2afa832e6c0b9856ae85a1b0be9af6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-28 19:31:10 -0300

    test: should fail to login if email is invalid

--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { usersMock } from './mocks';
 import { Model } from 'sequelize';
-import { OK } from '../utils/httpStatusCodes';
+import { BAD_REQUEST, OK } from '../utils/httpStatusCodes';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -17,11 +17,11 @@ describe('Integration tests for route /login and /users', function () {
   describe('login method', () => {
     it('should be able to login', async function () {
       sinon.stub(Model, 'findOne').resolves(usersMock.user);
-      
+
       const body = {
         email: usersMock.user.email,
         password: usersMock.user.password,
-      }
+      };
 
       const response = await chai.request(app).post('/login').send(body);
 
@@ -29,18 +29,15 @@ describe('Integration tests for route /login and /users', function () {
       expect(response.status).to.equal(OK);
     });
 
-    it('should fail to login if user is invalid', async function () {
+    it('should fail to login if email is invalid', async function () {
       sinon.stub(Model, 'findOne').resolves(null);
-      
-      const body = {
-        email: 'invalid',
-        password: usersMock.user.password,
-      }
 
-      const response = await chai.request(app).post('/login').send(body);
+      const errorMessage = { message: 'All fields must be filled' };
+      const response = await chai.request(app).post('/login').send(usersMock.invalidLoginInfo);
 
-      expect(response.body).to.deep.equal(usersMock.user.password);
-      expect(response.status).to.equal(OK);
+      expect(response.body).to.deep.equal(errorMessage);
+      expect(response.status).to.equal(BAD_REQUEST);
     });
+
   });
 });
