c3ff0d9c73fc8e133cfaa00476896e648a5ab3e7
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-28 19:34:00 -0300

    refactor: interfaces barrel import

---
+++ app/backend/src/api/interfaces/index.ts
@@ -0,0 +1,13 @@
+import ILoginInfo from './ILoginInfo';
+import ITeamController from './ITeamController';
+import ITeamService from './ITeamService';
+import IUserController from './IUserController';
+import IUserService from './IUserService';
+
+export {
+  ILoginInfo,
+  ITeamController,
+  ITeamService,
+  IUserController,
+  IUserService,
+};
