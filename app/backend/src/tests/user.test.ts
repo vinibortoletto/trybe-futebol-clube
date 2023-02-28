bb08651426c402609411b67ff77cb47e062a2dae
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-28 16:09:11 -0300

    test: should be able to login

---
+++ app/backend/src/tests/user.test.ts
@@ -0,0 +1,32 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import { usersMock } from './mocks';
+import { Model } from 'sequelize';
+import { OK } from '../utils/httpStatusCodes';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Integration tests for route /login and /users', function () {
+  afterEach(() => sinon.restore());
+
+  describe('login method', () => {
+    it('should be able to login', async function () {
+      sinon.stub(Model, 'findOne').resolves(usersMock.user);
+      
+      const body = {
+        email: usersMock.user.email,
+        password: usersMock.user.password,
+      }
+
+      const response = await chai.request(app).post('/login').send(body);
+
+      expect(response.body).to.deep.equal(usersMock.user.password);
+      expect(response.status).to.equal(OK);
+    });
+  });
+});
