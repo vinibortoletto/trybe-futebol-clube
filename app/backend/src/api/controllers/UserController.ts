0d0992a709f36eb296b2e837074dc1702208c386
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-28 20:23:05 -0300

    feat: try/catch in login request

--- app/backend/src/api/controllers/UserController.ts
+++ app/backend/src/api/controllers/UserController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { OK } from '../../utils/httpStatusCodes';
 import IUserController from '../interfaces/IUserController';
 import IUserService from '../interfaces/IUserService';
@@ -6,8 +6,12 @@ import IUserService from '../interfaces/IUserService';
 export default class UserController implements IUserController {
   constructor(private _service: IUserService) {}
 
-  async login(req: Request, res: Response) {
-    const token = await this._service.login(req.body);
-    return res.status(OK).json(token);
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = await this._service.login(req.body);
+      return res.status(OK).json(token);
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- app/backend/src/api/interfaces/IUserController.ts
+++ app/backend/src/api/interfaces/IUserController.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export default interface IUserController {
-  login(req:Request, res: Response): Promise<Response | void>
+  login(req:Request, res: Response, next: NextFunction): Promise<Response | void>
 }
