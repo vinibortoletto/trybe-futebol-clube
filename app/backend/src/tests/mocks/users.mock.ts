0c93bb93c33a21dfdf0380ff4134afc376434b04
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-28 19:31:30 -0300

    feat: invalid login mock

--- app/backend/src/tests/mocks/users.mock.ts
+++ app/backend/src/tests/mocks/users.mock.ts
@@ -1,16 +1,22 @@
 import User from '../../database/models/UserModel'
 import ILoginInfo from '../../api/interfaces/ILoginInfo'
 
+const password = "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW"
+
 export const user: User = {
   email: "admin@admin.com",
   id: 1,
-  password: "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW",
+  password,
   role: "admin",
   username: "Admin",
 } as unknown as User;
 
-export const loginInfo: ILoginInfo = {
+export const validLoginInfo: ILoginInfo = {
   email: "admin@admin.com",
-  password: "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW",
+  password,
 };
 
+export const invalidLoginInfo: ILoginInfo = {
+  email: '@invalid.com',
+  password,
+};
