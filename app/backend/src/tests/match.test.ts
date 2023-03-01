900bcf9a0884167af1bbf824828ad599b06a598d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 19:30:37 -0300

    test: should find all teams

---
+++ app/backend/src/tests/match.test.ts
@@ -0,0 +1,25 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const { expect } = chai;
+
+import { app } from '../app';
+import { Model } from 'sequelize';
+import { matchesMock } from './mocks';
+import { OK } from '../utils/httpStatusCodes';
+
+describe('Integration tests for route /matches', function () {
+  afterEach(() => sinon.restore());
+
+  describe('findAll method', function () {
+    it('should find all teams', async function () {
+      sinon.stub(Model, 'findAll').resolves(matchesMock.matchList);
+      const response = await chai.request(app).get('/matches').send();
+      expect(response.body).to.deep.equal(matchesMock.matchList);
+      expect(response.status).to.equal(OK);
+    });
+  });
+});
