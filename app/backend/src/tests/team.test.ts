66c32d7c52a70211795080cbac93431c50737c8e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 15:56:24 -0300

    test: should throw error 500 when trying to find all teams

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -6,7 +6,8 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { teamsMock } from './mocks';
 import { Model } from 'sequelize';
-import { OK } from '../utils/httpStatusCodes';
+import { INTERNAL_SERVER_ERROR, NOT_FOUND, OK } from '../utils/httpStatusCodes';
+import { teamNotFound } from '../utils/errorMessages';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -20,6 +21,14 @@ describe('Integration tests for route /teams', function () {
       const response = await chai.request(app).get('/teams').send();
       expect(response.body).to.deep.equal(teamsMock.teamList);
       expect(response.status).to.equal(OK);
+    });
+
+    it('should throw error 500 when trying to find all teams', async function () {
+      const errorMessage= 'Internal Server Error'
+      sinon.stub(Model, 'findAll').rejects(new Error(errorMessage));
+      const response = await chai.request(app).get('/teams').send()
+      expect(response.body).to.deep.equal({message: errorMessage});
+      expect(response.status).to.equal(INTERNAL_SERVER_ERROR);
     });
   });
 
