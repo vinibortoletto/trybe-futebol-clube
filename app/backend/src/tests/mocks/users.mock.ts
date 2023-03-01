309b5dc077b5adea1e5e2df19f6274b23b1b3b39
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 18:37:31 -0300

    refactor: token mock name

--- app/backend/src/tests/mocks/users.mock.ts
+++ app/backend/src/tests/mocks/users.mock.ts
@@ -31,4 +31,4 @@ export const wrongLoginInfo: ILoginInfo = {
   password
 }
 
-export const invalidToken: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc'
+export const token: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc'
