e5d97a2d2962bc8f4015243238b8136ae707ac07
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 19:31:49 -0300

    chore: interface import

--- app/backend/src/api/interfaces/index.ts
+++ app/backend/src/api/interfaces/index.ts
@@ -1,8 +1,13 @@
 import ILoginInfo from './ILoginInfo';
+
 import ITeamController from './ITeamController';
 import ITeamService from './ITeamService';
+
 import IUserController from './IUserController';
 import IUserService from './IUserService';
+
+import IMatchController from './IMatchController';
+import IMatchService from './IMatchService';
 
 export {
   ILoginInfo,
@@ -10,4 +15,6 @@ export {
   ITeamService,
   IUserController,
   IUserService,
+  IMatchController,
+  IMatchService,
 };
