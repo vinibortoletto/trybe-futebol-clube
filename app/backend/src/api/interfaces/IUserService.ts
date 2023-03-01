3ef85a93aea4670143dbbf50a7499148d17de877
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 18:38:04 -0300

    feat: getRole service

--- app/backend/src/api/interfaces/IUserService.ts
+++ app/backend/src/api/interfaces/IUserService.ts
@@ -1,5 +1,7 @@
+import User from '../../database/models/UserModel';
 import TLoginInfo from './ILoginInfo';
 
 export default interface IUserService {
   login(loginInfo: TLoginInfo): Promise<string>
+  getRole(userInfo: User): Promise<string | void>
 }
--- app/backend/src/api/services/UserService.ts
+++ app/backend/src/api/services/UserService.ts
@@ -1,13 +1,11 @@
 import { ModelStatic } from 'sequelize';
 import * as bcrypt from 'bcryptjs';
-import { Secret, sign } from 'jsonwebtoken';
 import IUserService from '../interfaces/IUserService';
 import ILoginInfo from '../interfaces/ILoginInfo';
 import User from '../../database/models/UserModel';
 import { Unauthorized } from '../errors';
 import { invalidFields } from '../../utils/errorMessages';
-
-const secret = process.env.JWT_SECRET || undefined as unknown as Secret;
+import TokenHandler from '../../utils/TokenHandler';
 
 export default class UserService implements IUserService {
   private _model: ModelStatic<User> = User;
@@ -18,10 +16,15 @@ export default class UserService implements IUserService {
     const user = await this._model.findOne({ where: { email } });
     if (!user) throw new Unauthorized(invalidFields);
 
-    const isPasswordValid = bcrypt.compareSync(password, user?.password);
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
     if (!isPasswordValid) throw new Unauthorized(invalidFields);
 
-    const token = sign(loginInfo, secret);
+    const token = TokenHandler.generate(loginInfo);
     return token;
+  }
+
+  public async getRole(userInfo: User): Promise<string | void> {
+    const user = await this._model.findOne({ where: { email: userInfo.email } });
+    return user?.role;
   }
 }
