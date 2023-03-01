6e7caa8671e42b4e4f2cf2c3ba652597b5c94a1c
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 15:57:45 -0300

    fix: try/catch for team controller

--- app/backend/src/api/controllers/TeamController.ts
+++ app/backend/src/api/controllers/TeamController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { OK } from '../../utils/httpStatusCodes';
 import ITeamController from '../interfaces/ITeamController';
 import ITeamService from '../interfaces/ITeamService';
@@ -6,14 +6,23 @@ import ITeamService from '../interfaces/ITeamService';
 export default class TeamController implements ITeamController {
   constructor(private _service: ITeamService) {}
 
-  async findAll(_req: Request, res: Response) {
-    const teamList = await this._service.findAll();
-    return res.status(OK).json(teamList);
+  async findAll(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const teamList = await this._service.findAll();
+      return res.status(OK).json(teamList);
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async findById(req: Request, res: Response) {
+  async findById(req: Request, res: Response, next: NextFunction) {
     const { id } = req.params;
-    const team = await this._service.findById(Number(id));
-    return res.status(OK).json(team);
+
+    try {
+      const team = await this._service.findById(Number(id));
+      return res.status(OK).json(team);
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- app/backend/src/api/interfaces/ITeamController.ts
+++ app/backend/src/api/interfaces/ITeamController.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export default interface ITeamController {
-  findAll(req: Request, res: Response): Promise<Response | void>
-  findById(req: Request, res: Response): Promise<Response | void>
+  findAll(req: Request, res: Response, next: NextFunction): Promise<Response | void>
+  findById(req: Request, res: Response, next: NextFunction): Promise<Response | void>
 }
