f804419e1be2bfae73f4cd35e795c604ac990af7
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 17:25:20 -0300

    chore: token error messages

--- app/backend/src/utils/errorMessages.ts
+++ app/backend/src/utils/errorMessages.ts
@@ -1,3 +1,5 @@
 export const requiredFields = 'All fields must be filled';
 export const invalidFields = 'Invalid email or password';
 export const teamNotFound = 'Team not found';
+export const tokenNotFound = 'Token not found';
+export const invalidToken = 'Token must be a valid token';
