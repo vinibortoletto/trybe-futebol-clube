779b878a9ccfe83eb02b2f45fba9b2013c8df174
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 18:38:11 -0300

    feat: getRole controller

--- app/backend/src/api/controllers/UserController.ts
+++ app/backend/src/api/controllers/UserController.ts
@@ -6,10 +6,21 @@ import IUserService from '../interfaces/IUserService';
 export default class UserController implements IUserController {
   constructor(private _service: IUserService) {}
 
-  async login(req: Request, res: Response, next: NextFunction) {
+  public async login(req: Request, res: Response, next: NextFunction) {
     try {
       const token = await this._service.login(req.body);
       return res.status(OK).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async getRole(req: Request, res: Response, next: NextFunction) {
+    const { user } = req.body;
+
+    try {
+      const role = await this._service.getRole(user);
+      return res.status(OK).json({ role });
     } catch (error) {
       next(error);
     }
