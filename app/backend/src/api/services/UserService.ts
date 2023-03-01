a1ab24207503386624c6f3bd469ef006ae96728e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-28 22:01:46 -0300

    feat: login validation

--- app/backend/src/api/services/UserService.ts
+++ app/backend/src/api/services/UserService.ts
@@ -1,24 +1,27 @@
 import { ModelStatic } from 'sequelize';
 import * as bcrypt from 'bcryptjs';
+import { Secret, sign } from 'jsonwebtoken';
 import IUserService from '../interfaces/IUserService';
 import ILoginInfo from '../interfaces/ILoginInfo';
 import User from '../../database/models/UserModel';
 import { Unauthorized } from '../errors';
 import { invalidFields } from '../../utils/errorMessages';
 
+const secret = process.env.JWT_SECRET || undefined as unknown as Secret;
+
 export default class UserService implements IUserService {
   private _model: ModelStatic<User> = User;
 
   async login(loginInfo: ILoginInfo): Promise<string> {
     const { email, password } = loginInfo;
-    const user = await this._model.findOne({ where: { email } });
 
+    const user = await this._model.findOne({ where: { email } });
     if (!user) throw new Unauthorized(invalidFields);
 
     const isPasswordValid = bcrypt.compareSync(password, user?.password);
-
     if (!isPasswordValid) throw new Unauthorized(invalidFields);
 
-    return password;
+    const token = sign(loginInfo, secret);
+    return token;
   }
 }
