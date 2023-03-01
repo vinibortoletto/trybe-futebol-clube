817dd65901b005be3cd26d7839702254017da98d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-01 17:25:48 -0300

    feat: GET /login/role route

--- app/backend/src/api/routers/usersRouter.ts
+++ app/backend/src/api/routers/usersRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
 import { ValidateLogin } from '../middlewares';
+import ValidateToken from '../middlewares/ValidateToken';
 import UserService from '../services/UserService';
 
 const router = Router();
@@ -8,5 +9,6 @@ const service = new UserService();
 const controller = new UserController(service);
 
 router.post('/login', ValidateLogin.validate, controller.login.bind(controller));
+router.get('/login/role', ValidateToken.validate, controller.getRole.bind(controller));
 
 export default router;
