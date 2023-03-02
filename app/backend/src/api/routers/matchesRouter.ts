57bbf037d9601b7b3f84dc79c75961f84b931d36
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-02 16:52:02 -0300

    feat: PATCH /matches/:id/finish route

--- app/backend/src/api/routers/matchesRouter.ts
+++ app/backend/src/api/routers/matchesRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
+import ValidateToken from '../middlewares/ValidateToken';
 import MatchService from '../services/MatchService';
 
 const router = Router();
@@ -7,5 +8,11 @@ const service = new MatchService();
 const controller = new MatchController(service);
 
 router.get('/matches', controller.findAll.bind(controller));
+
+router.patch(
+  '/matches/:id/finish',
+  ValidateToken.validate,
+  controller.finish.bind(controller),
+);
 
 export default router;
