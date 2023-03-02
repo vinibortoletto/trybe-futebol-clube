dcebfe4c313ddeabc5c630b8ed72b6de41e8f139
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-02 18:37:34 -0300

    test: should be able to create a new match

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -9,7 +9,7 @@ const { expect } = chai;
 import { app } from '../app';
 import { Model } from 'sequelize';
 import { matchesMock, usersMock } from './mocks';
-import { OK } from '../utils/httpStatusCodes';
+import { CREATED, OK } from '../utils/httpStatusCodes';
 
 describe('Integration tests for route /matches', function () {
   afterEach(() => sinon.restore());
@@ -48,6 +48,21 @@ describe('Integration tests for route /matches', function () {
 
       expect(response.body).to.deep.equal({ message: 'Updated' });
       expect(response.status).to.equal(OK);
+    });
+  });
+
+  describe('create method', function () {
+    it.only('should be able to create a new match', async function () {
+      sinon.stub(Model, 'create').resolves(matchesMock.matchResponse);
+
+      const response = await chai
+        .request(app)
+        .post('/matches')
+        .send(matchesMock.matchBody)
+        .set('Authorization', usersMock.validToken);
+
+      expect(response.body).to.deep.equal(matchesMock.matchResponse);
+      expect(response.status).to.equal(CREATED);
     });
   });
 });
