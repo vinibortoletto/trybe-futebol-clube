30067edcf804de3fb1862c8dd61b3695ec302631
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-02 18:25:22 -0300

    feat: match interface

--- app/backend/src/api/interfaces/IMatch.ts
+++ app/backend/src/api/interfaces/IMatch.ts
@@ -1,4 +1,8 @@
-export default interface IMatch {
-  homeTeamGoals: number
-  awayTeamGoals: number
+import IGoals from './IGoals';
+
+export default interface IMatch extends IGoals {
+  homeTeamId: number;
+  awayTeamId: number;
+  homeTeamGoals: number;
+  awayTeamGoals: number;
 }
