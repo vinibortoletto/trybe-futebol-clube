947b434b1acdb1ef4a1b69b11a0c192c10e88305
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-02 18:55:57 -0300

    chore: http status

--- app/backend/src/utils/httpStatusCodes.ts
+++ app/backend/src/utils/httpStatusCodes.ts
@@ -3,4 +3,5 @@ export const CREATED = 201;
 export const BAD_REQUEST = 400;
 export const UNAUTHORIZED = 401;
 export const NOT_FOUND = 404;
+export const UNPROCESSABLE_CONTENT = 422;
 export const INTERNAL_SERVER_ERROR = 500;
