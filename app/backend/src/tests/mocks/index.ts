e4edfe015c20204597a546eb8f3bbe755ef2bb7b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-03 20:46:05 -0300

    chore: leaderboard mocks

--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -1,5 +1,11 @@
 import * as teamsMock from './teams.mock';
 import * as usersMock from './users.mock';
 import * as matchesMock from './matches.mock';
+import * as leaderboardMock from './leaderboard.mock';
 
-export { teamsMock, usersMock, matchesMock };
+export { 
+  teamsMock, 
+  usersMock, 
+  matchesMock,
+  leaderboardMock
+};
