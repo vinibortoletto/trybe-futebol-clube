fc9d4059e1863e17ecefd2ea34a8a283a365b8b1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-02 22:10:17 -0300

    feat: create match validation

--- app/backend/src/api/services/MatchService.ts
+++ app/backend/src/api/services/MatchService.ts
@@ -1,18 +1,18 @@
 import { ModelStatic } from 'sequelize';
-import { sameTeams } from '../../utils/errorMessages';
+import { invalidTeam, sameTeams } from '../../utils/errorMessages';
 import Team from '../../database/models/TeamModel';
 import Match from '../../database/models/MatchModel';
 import { IMatch, IMatchService } from '../interfaces';
 import IGoals from '../interfaces/IGoals';
 import UnprocessableContent from '../errors/UnprocessableContent';
+import NotFound from '../errors/NotFound';
 
 export default class MatchService implements IMatchService {
   private _model: ModelStatic<Match> = Match;
+  private static _teamModel: ModelStatic<Team> = Team;
 
   public async findAll(inProgress: string): Promise<Match[]> {
-    const condition = inProgress
-      ? { inProgress: JSON.parse(inProgress) }
-      : {};
+    const condition = inProgress ? { inProgress: JSON.parse(inProgress) } : {};
 
     const matchList = await this._model.findAll({
       include: [
@@ -26,17 +26,15 @@ export default class MatchService implements IMatchService {
   }
 
   public async finish(id: number): Promise<string> {
-    await this._model.update(
-      { isProgress: false },
-      { where: { id } },
-    );
+    await this._model.update({ isProgress: false }, { where: { id } });
 
     return 'Finished';
   }
 
   public async update(goals: IGoals, id: number): Promise<string> {
     await this._model.update(
-      { homeTeamGoals: goals.homeTeamGoals,
+      {
+        homeTeamGoals: goals.homeTeamGoals,
         awayTeamGoals: goals.awayTeamGoals,
       },
       { where: { id } },
@@ -45,13 +43,31 @@ export default class MatchService implements IMatchService {
     return 'Updated';
   }
 
-  private static validateMatch({ homeTeamId, awayTeamId }: IMatch): void {
+  private static validateIfTeamsAreTheSame({
+    homeTeamId,
+    awayTeamId,
+  }: IMatch): void {
     const areTeamsTheSame = homeTeamId === awayTeamId;
     if (areTeamsTheSame) throw new UnprocessableContent(sameTeams);
   }
 
+  private static async validateIfTeamsExist({
+    homeTeamId,
+    awayTeamId,
+  }: IMatch): Promise<void> {
+    const promises = [homeTeamId, awayTeamId].map(async (id) =>
+      this._teamModel.findByPk(id));
+
+    const teamList = await Promise.all(promises);
+
+    if (teamList.some((team) => !team)) {
+      throw new NotFound(invalidTeam);
+    }
+  }
+
   public async create(newMatch: IMatch): Promise<Match> {
-    MatchService.validateMatch(newMatch);
+    MatchService.validateIfTeamsAreTheSame(newMatch);
+    await MatchService.validateIfTeamsExist(newMatch);
 
     const match = await this._model.create({ ...newMatch, inProgress: true });
     return match;
