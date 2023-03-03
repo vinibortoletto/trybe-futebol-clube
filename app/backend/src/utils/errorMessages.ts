fd0c6820371fd1e58e5897cbbf4fe6f881067066
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-02 22:01:04 -0300

    chore: error message

--- app/backend/src/utils/errorMessages.ts
+++ app/backend/src/utils/errorMessages.ts
@@ -4,3 +4,4 @@ export const teamNotFound = 'Team not found';
 export const tokenNotFound = 'Token not found';
 export const invalidToken = 'Token must be a valid token';
 export const sameTeams = 'It is not possible to create a match with two equal teams';
+export const invalidTeam = 'There is no team with such id!';
