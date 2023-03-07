c20bbd69a3e31e994fc868e1bbd0a45cdcb8470d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-07 01:14:22 -0300

    refactor error barrel import

--- app/backend/src/api/errors/index.ts
+++ app/backend/src/api/errors/index.ts
@@ -1,4 +1,6 @@
 import BadRequest from './BadRequest';
 import Unauthorized from './Unauthorized';
+import NotFound from './NotFound';
+import UnprocessableContent from './UnprocessableContent';
 
-export { BadRequest, Unauthorized };
+export { BadRequest, Unauthorized, NotFound, UnprocessableContent };
