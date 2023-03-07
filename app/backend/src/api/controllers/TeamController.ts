00c7c42245d89eeffc9f7b0527c71d5e482fbe7d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-07 00:45:27 -0300

    refactor: team controller types

--- app/backend/src/api/controllers/TeamController.ts
+++ app/backend/src/api/controllers/TeamController.ts
@@ -1,25 +1,32 @@
 import { NextFunction, Request, Response } from 'express';
 import { OK } from '../../utils/httpStatusCodes';
-import ITeamController from '../interfaces/ITeamController';
-import ITeamService from '../interfaces/ITeamService';
+import { ITeam, ITeamController, ITeamService } from '../interfaces';
 
 export default class TeamController implements ITeamController {
   constructor(private _service: ITeamService) {}
 
-  async findAll(_req: Request, res: Response, next: NextFunction) {
+  async findAll(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> {
     try {
-      const teamList = await this._service.findAll();
+      const teamList: ITeam[] = await this._service.findAll();
       return res.status(OK).json(teamList);
     } catch (error) {
       next(error);
     }
   }
 
-  async findById(req: Request, res: Response, next: NextFunction) {
+  async findById(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> {
     const { id } = req.params;
 
     try {
-      const team = await this._service.findById(Number(id));
+      const team: ITeam = await this._service.findById(Number(id));
       return res.status(OK).json(team);
     } catch (error) {
       next(error);
