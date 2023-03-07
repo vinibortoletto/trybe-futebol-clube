1760e715d6126f4297756c1047acc12345fe3638
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-07 00:38:24 -0300

    refactor: leaderboard controller types

--- app/backend/src/api/controllers/LeaderboardController.ts
+++ app/backend/src/api/controllers/LeaderboardController.ts
@@ -1,17 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import { OK } from '../../utils/httpStatusCodes';
-import { ILeaderboardController, ILeaderboardService } from '../interfaces';
+import { ILeaderboardController, ILeaderboardRow, ILeaderboardService } from '../interfaces';
 import TTeamType from '../types/TTeamType';
 
 export default class LeaderboardController implements ILeaderboardController {
   constructor(private _service: ILeaderboardService) {}
 
-  public async getLeaderboard(req: Request, res: Response, next: NextFunction) {
+  public async getLeaderboard(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> {
     const route: string = req.url.replace(/\//g, '').replace('leaderboard', '');
     const teamType: string = route || 'both';
 
     try {
-      const leaderboardList = await this._service.getLeaderboard(teamType as TTeamType);
+      const leaderboardList: ILeaderboardRow[] = await this._service
+        .getLeaderboard(teamType as TTeamType);
       return res.status(OK).json(leaderboardList);
     } catch (error) {
       next(error);
