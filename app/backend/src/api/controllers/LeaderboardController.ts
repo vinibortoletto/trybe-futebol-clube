92f5fb579388c39a4bd385e928b92dde3fc87dd9
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-06 22:17:30 -0300

    refactor: leaderboard controller

--- app/backend/src/api/controllers/LeaderboardController.ts
+++ app/backend/src/api/controllers/LeaderboardController.ts
@@ -1,31 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { OK } from '../../utils/httpStatusCodes';
 import { ILeaderboardController, ILeaderboardService } from '../interfaces';
+import TTeamType from '../types/TTeamType';
 
 export default class LeaderboardController implements ILeaderboardController {
   constructor(private _service: ILeaderboardService) {}
 
-  public async getHomeLeaderboard(_req: Request, res: Response, next: NextFunction) {
-    try {
-      const leaderboardList = await this._service.getHomeLeaderboard();
-      return res.status(OK).json(leaderboardList);
-    } catch (error) {
-      next(error);
-    }
-  }
+  public async getLeaderboard(req: Request, res: Response, next: NextFunction) {
+    const route = req.url.replace(/\//g, '').replace('leaderboard', '');
+    const teamType = route || 'both';
+    console.log(route);
 
-  public async getAwayLeaderboard(_req: Request, res: Response, next: NextFunction) {
     try {
-      const leaderboardList = await this._service.getAwayLeaderboard();
-      return res.status(OK).json(leaderboardList);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async getLeaderboard(_req: Request, res: Response, next: NextFunction) {
-    try {
-      const leaderboardList = await this._service.getLeaderboard();
+      const leaderboardList = await this._service.getLeaderboard(teamType as TTeamType);
       return res.status(OK).json(leaderboardList);
     } catch (error) {
       next(error);
