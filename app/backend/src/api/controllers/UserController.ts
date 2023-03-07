ae3de97ff08909893d228b9793e3ad701d548676
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-07 00:56:08 -0300

    refactor: user controller types

--- app/backend/src/api/controllers/UserController.ts
+++ app/backend/src/api/controllers/UserController.ts
@@ -6,20 +6,28 @@ import IUserService from '../interfaces/IUserService';
 export default class UserController implements IUserController {
   constructor(private _service: IUserService) {}
 
-  public async login(req: Request, res: Response, next: NextFunction) {
+  public async login(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> {
     try {
-      const token = await this._service.login(req.body);
+      const token: string = await this._service.login(req.body);
       return res.status(OK).json({ token });
     } catch (error) {
       next(error);
     }
   }
 
-  public async getRole(req: Request, res: Response, next: NextFunction) {
+  public async getRole(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> {
     const { user } = req.body;
 
     try {
-      const role = await this._service.getRole(user);
+      const role: string | undefined = await this._service.getRole(user);
       return res.status(OK).json({ role });
     } catch (error) {
       next(error);
